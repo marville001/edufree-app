a929e9a9345e307c93699c05c14f696a8d7388c4
AlbrightSunguti <albright.sunguti@abxtractventurelabs.com>
2023-03-09 16:54:54 +0300

    task:create login and register components

--- client/src/app/modules/auth/auth.module.ts
+++ client/src/app/modules/auth/auth.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    RegisterComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
