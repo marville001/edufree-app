020c717de50295192f07bcd3c4cb843c79e3e379
Martin Mwangi <mwangimartin1904@gmail.com>
2023-03-12 12:37:18 +0300

    Add jwt config

--- api/api.auth/src/config/config.ts
+++ api/api.auth/src/config/config.ts
@@ -3,8 +3,10 @@ dotenv.config();
 
 const PORT = process.env.PORT || 8800
 const DB_URL = process.env.DB_URL || "";
+const JWT_SECRET = process.env.JWT_SECRET as string
 
 export const config = {
 	PORT,
-	DB_URL
+	DB_URL,
+	JWT_SECRET
 }
